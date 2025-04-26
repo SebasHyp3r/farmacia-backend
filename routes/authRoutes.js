336aefc9699fb9b8fd02cdaafcc4b5d6e4b0588b
SebasHyp3r <sebashyp3r@gmail.com>
2025-04-26 03:30:13 -0500

    proceso

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,18 @@
 console.log("authRoutes cargado correctamente");
+
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
 
+// Ruta de prueba
 router.get('/test', (req, res) => {
     res.send('Ruta de prueba OK');
 });
 
-// Estas son tus rutas reales:
+// Ruta para registrar empleados
 router.post('/register', register);
+
+// Ruta para login de empleados
 router.post('/login', login);
 
 module.exports = router;
