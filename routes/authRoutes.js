b6cf452c87e135dda398590a0f948fc1eceed2be
SebasHyp3r <sebashyp3r@gmail.com>
2025-04-26 03:16:59 -0500

    proceso

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,10 @@ router.get('/test', (req, res) => {
     res.send('Ruta de prueba OK');
   });
   
-
+  app.post('/api/auth/register', (req, res) => {
+    res.send('Registro temporal funcionando');
+  });
+  
 // Ruta para registrar empleados
 router.post('/register', register);
 
