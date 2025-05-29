06cab0525fc8b85fbb058a0f3ff50e8b4b102a4b
SebasHyp3r <sebashyp3r@gmail.com>
2025-05-29 13:49:43 -0500

    22

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const { verificarToken, soloAdmin } = require('../middleware/authMiddleware');
+const { verificarToken, soloAdmin } = require('../middlewares/authMiddleware');
 
 // Ruta protegida, solo admins pueden listar usuarios
 router.get('/', verificarToken, soloAdmin, async (req, res) => {
